f83e0cedf774a046bee0e682487d15871070f34f
Mohammad Hider <mhider2006@gmail.com>
2025-04-22 20:18:38 +0000

    changing app.py to test in codespace

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return jsonify({"message": "Hello from Flask backend!"})
+    return jsonify({"message": "This is mohammad from the backend"})
 
 @app.route('/api/test')
 def test():
